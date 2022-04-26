28e3db47aefe61e4363d4c4c80b430ae5efb8920
Rainer Regan <rainercoc@gmail.com>
2022-04-26 17:49:31 +0700

    Menambahkan Background

--- core/src/com/rainerregan/flappybird/FlappyBird.java
+++ core/src/com/rainerregan/flappybird/FlappyBird.java
@@ -1,31 +1,25 @@
 package com.rainerregan.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background; // Seperti image
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		batch = new SpriteBatch(); // Managing Sprites
+		background = new Texture("bg.png"); // Mengambil sumber dari bg.png
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		batch.begin(); // Begin Batch: Memberi tahu render untuk memulai
+		batch.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.end(); // End render
 	}
 }
